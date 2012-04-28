045b27b404bc6ca5ebda2f729eddccb82943a0fb
Cheng Zeng <Vvnesaa@gmail.com>
2012-04-28 03:51:33 -0400

    change to multithread multicast

--- core/send_message.py
+++ core/send_message.py
@@ -2,6 +2,7 @@ import socket
 import cPickle as pickle
 import struct
 import time
+import threading
 
 # first send length, then send pickled data
 def send_tcp_msg(conn, data):
@@ -14,7 +15,7 @@ def send_tcp_msg(conn, data):
 	except Exception, exc:
 		print "Exception: {} send tcp msg".format(exc)
 
-def multicastMove(grid, gvar):
+def multicastMove_old(grid, gvar):
 	print "Multicast Movement Msg"
 	gvar.lock.acquire()
 	playerPos = gvar.playerPos
@@ -25,3 +26,20 @@ def multicastMove(grid, gvar):
 				playerPos[x].last_stime = time.time()
 				send_tcp_msg(conn, (6, grid))
 	gvar.lock.release()
+
+def domultisend(grid, gvar):
+	print "Multicast Movement Msg"
+	gvar.lock.acquire()
+	playerPos = gvar.playerPos
+	for x in playerPos.iterkeys():
+		if x != gvar.myID:
+			conn = playerPos[x].conn
+			if conn is not None:
+				playerPos[x].last_stime = time.time()
+				send_tcp_msg(conn, (6, grid))
+	gvar.lock.release()
+
+def multicastMove(grid, gvar):
+	temp = threading.Thread(target = domultisend, kwargs = {"grid":grid, "gvar":gvar})
+	temp.daemon = True
+	temp.start()
